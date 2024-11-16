63824b594e6dc5622a96f7d16c41afeb119f2284
Gian Klug <gian.klug@adfinis.com>
2024-11-16 12:14:51 +0100

    feat: fix shell exitting, automatic dir flattening, better shell matching, chdir in ctfmv too

--- install.sh
+++ install.sh
@@ -2,15 +2,26 @@
 
 cp {ctf,ctfmv} "$HOME/.local/bin/"
 
+shopt -s extglob
+
+# Ensure fzf is installed
+if ! command -v fzf &> /dev/null; then
+    echo "fzf is not installed. Please install fzf to use this script."
+    return
+fi
+
 case $SHELL in
-    "/usr/bin/bash")
+    *bash)
         echo "alias ctf='source ctf'" >> "$HOME/.bashrc"
+        echo "alias ctfmv='source ctfmv'" >> "$HOME/.bashrc"
         ;;
-    "/usr/bin/zsh")
+    *zsh)
         echo "alias ctf='source ctf'" >> "$HOME/.zshrc"
+        echo "alias ctfmv='source ctfmv'" >> "$HOME/.zshrc"
         ;;
     *)
-        echo "Unknown shell. Failed to create alias for ctf='source ctf'."
+        echo "Unknown shell. Failed to create aliases for ctf='source ctf' and ctfmv='source ctfmv'."
         ;;
 esac
 
+echo "Installation done. Run 'ctfmv <challengename>' to move files and 'ctf <challengename>' to switch to a ctf directory."
